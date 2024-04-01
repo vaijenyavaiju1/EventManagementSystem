6c2b9788a3051937c90cd419271eb16e47d90391
vaijenyavaiju1 <vaijenyavaiju123@gamil.com>
2024-04-01 04:52:41 +0000

    solution

---
+++ src/main/java/com/example/eventmanagementsystem/repository/EventRepository.java
@@ -0,0 +1,19 @@
+package com.example.eventmanagementsystem.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.eventmanagementsystem.model.Sponsor;
+import com.example.eventmanagementsystem.model.Event;
+
+public interface EventRepository {
+    ArrayList<Event> getEvents();
+
+    Event getEventById(int eventId);
+
+    Event addEvent(Event event);
+
+    void deleteEvent(int eventId);
+
+    List<Sponsor> getEventSponsor(int eventId);
+}
